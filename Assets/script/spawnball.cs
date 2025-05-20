ddb06558fbab554bbbcb617237bbb0eddd8db7e4
Kenzel <kenzelkdsouza@gmail.com>
2025-05-20 14:53:49 +0400

    Initial commit

---
+++ Assets/script/spawnball.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Oculus;
+public class spawnball : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    //public Transform shootingPoint;  // <--- assign this in the inspector
+    public float force = 5f;
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            SpawnBall();
+        }
+    }
+
+    void SpawnBall()
+    {
+   
+        GameObject ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * force, ForceMode.Impulse);
+
+    }
+}
