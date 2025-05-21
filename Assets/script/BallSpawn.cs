9d5c82e453f02744eb925727482289ff1836dcef
Kenzel <aymanamar983@gmail.com>
2025-05-21 16:23:29 +0400

    changes

---
+++ Assets/script/BallSpawn.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class BallSpawn : MonoBehaviour
+{
+    public GameObject Prefab;
+    public float shootForce = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            shootBall();
+        }
+    }
+
+    void shootBall()
+    {
+        GameObject ball = Instantiate(Prefab, transform.position, transform.rotation);
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.AddForce(transform.forward * shootForce, ForceMode.Impulse);
+        }
+    }
+}
