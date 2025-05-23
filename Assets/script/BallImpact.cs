c872260aceae20b86d98d670a151dfee50c9fa2d
Kenzel <aymanamar983@gmail.com>
2025-05-23 10:34:56 +0400

    updated

---
+++ Assets/script/BallImpact.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BallImpact : MonoBehaviour
+{
+    public GameObject impactEffect;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+
+        
+        if (impactEffect != null)
+        {
+            Instantiate(impactEffect, contact.point, Quaternion.LookRotation(contact.normal));
+        }
+
+        Destroy(gameObject); 
+    }
+}
